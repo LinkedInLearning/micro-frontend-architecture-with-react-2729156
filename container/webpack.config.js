908db0968deaa047ef5747c55ca77e2ae8e898c2
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2025-02-22 22:24:01 +0000

    fix routing issues

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,22 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { use } = require('react');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: "./src/index.js",
+  mode: "development",
   devServer: {
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*",
     },
     port: 3000,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   output: {
-    publicPath: 'auto',
+    // publicPath: "auto",
+    publicPath: "http://localhost:3000/",
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
@@ -24,33 +24,41 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ]
-          }
-        }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"]
-      }
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
-        listing: "listing@localhost:3000/remoteEntry.js",
-        cart: "cart@https://special-funicular-q94rr9gqg4j364wg-3002.app.github.dev/remoteEntry.js",
-        checkout: "checkout@https://special-funicular-q94rr9gqg4j364wg-3003.app.github.dev/remoteEntry.js"
+        listing: "listing@http://localhost:3001/remoteEntry.js",
+        cart: "cart@http://localhost:3002/remoteEntry.js",
+        checkout: "checkout@http://localhost:3003/remoteEntry.js",
       },
-      shared: ['react', 'react-dom'],
+      // shared: ["react", "react-dom", "react-router-dom"],
+      shared: {
+        react: {
+          singleton: true,
+        },
+        "react-dom": {
+          singleton: true,
+        },
+        "react-router-dom": {
+          singleton: true,
+        },
+      }
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
