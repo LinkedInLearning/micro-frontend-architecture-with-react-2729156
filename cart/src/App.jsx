7220e7101a27f8cb0d08ff01d30ab4accdab0f25
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2025-02-22 22:15:25 +0000

    fix routing issues

--- cart/src/App.jsx
+++ cart/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CartPage from "./components/CartPage";
 
 const App = () => {
   return (
     <div className="bg-gray-900">
-      <Router>
-        <Routes>
-          <Route path="/" element={<CartPage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route index element={<CartPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
     </div>
   );
 };
